9fa22b115c09c5167374e72f635502b507a73b4b
nicop311 <18116277+nicop311@users.noreply.github.com>
2025-04-09 23:23:49 +0200

    move init config & viper from prerun to init and add persistent flags & regular flags for version
    Signed-off-by: nicop311 <18116277+nicop311@users.noreply.github.com>

--- cmd/version.go
+++ cmd/version.go
@@ -17,11 +17,20 @@ var versionFlag2 string
 var versionFlag3 string
 var versionFlag4 string
 
+var versionPersistentFlag1 string
+var versionPersistentFlag2 string
+var versionPersistentFlag3 string
+var versionPersistentFlag4 string
+
 type ViperFlagsVersion struct {
-	VersionFlag1 string `mapstructure:"versionflag1"`
-	VersionFlag2 string `mapstructure:"versionflag2"`
-	VersionFlag3 string `mapstructure:"versionflag3"`
-	VersionFlag4 string `mapstructure:"versionflag4"`
+	VersionFlag1           string `mapstructure:"versionflag1"`
+	VersionFlag2           string `mapstructure:"versionflag2"`
+	VersionFlag3           string `mapstructure:"versionflag3"`
+	VersionFlag4           string `mapstructure:"versionflag4"`
+	VersionPersistentFlag1 string `mapstructure:"versionpersistentflag1"`
+	VersionPersistentFlag2 string `mapstructure:"versionpersistentflag2"`
+	VersionPersistentFlag3 string `mapstructure:"versionpersistentflag3"`
+	VersionPersistentFlag4 string `mapstructure:"versionpersistentflag4"`
 }
 
 var vprFlgsVersion ViperFlagsVersion
@@ -35,20 +44,7 @@ var versionCmd = &cobra.Command{
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		vprBuf := viper.GetViper()
-		// Bind subcommand-specific cobra flags to viper
-		err := vprBuf.BindPFlags(cmd.Flags())
-		if err != nil {
-			logrus.WithField("cobra-cmd", cmd.Use).Errorf("error binding flags: %v", err)
-			os.Exit(1)
-		}
-
-		err = UnmarshalSubMerged(vprBuf, "version", &vprFlgsVersion)
-		if err != nil {
-			logrus.Fatalf("failed to unmarshal version config: %v", err)
-		}
-
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		return nil
 	},
 	Run: func(cmd *cobra.Command, args []string) {
@@ -58,16 +54,22 @@ to quickly create a Cobra application.`,
 		logrus.WithField("cobra-cmd", cmd.Use).Infof("versionflag3: %s", vprFlgsVersion.VersionFlag3)
 		logrus.WithField("cobra-cmd", cmd.Use).Infof("versionflag4: %s", vprFlgsVersion.VersionFlag4)
 
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("versionpersistentflag1: %s", vprFlgsVersion.VersionPersistentFlag1)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("versionpersistentflag2: %s", vprFlgsVersion.VersionPersistentFlag2)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("versionpersistentflag3: %s", vprFlgsVersion.VersionPersistentFlag3)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("versionpersistentflag4: %s", vprFlgsVersion.VersionPersistentFlag4)
+
 		fmt.Println("")
 		logrus.WithField("cobra-cmd", cmd.Use).Infof("Persistent flags from rootCmd")
-		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootflag1: %s", vprFlgsRoot.RootFlag1)
-		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootflag2: %s", vprFlgsRoot.RootFlag2)
-		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootflag3: %s", vprFlgsRoot.RootFlag3)
-		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootflag4: %s", vprFlgsRoot.RootFlag4)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootpersistentflag1: %s", vprFlgsRoot.RootPersistentFlag1)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootpersistentflag2: %s", vprFlgsRoot.RootPersistentFlag2)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootpersistentflag3: %s", vprFlgsRoot.RootPersistentFlag3)
+		logrus.WithField("cobra-cmd", cmd.Use).Infof("rootpersistentflag4: %s", vprFlgsRoot.RootPersistentFlag4)
 	},
 }
 
 func init() {
+	cobra.OnInitialize(initConfigVersion)
 	rootCmd.AddCommand(versionCmd)
 
 	// Here you will define your flags and configuration settings.
@@ -75,6 +77,10 @@ func init() {
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+	versionCmd.PersistentFlags().StringVar(&versionPersistentFlag1, "versionpersistentflag1", "value from default", "version persistent flag 1")
+	versionCmd.PersistentFlags().StringVar(&versionPersistentFlag2, "versionpersistentflag2", "value from default", "version persistent flag 2")
+	versionCmd.PersistentFlags().StringVar(&versionPersistentFlag3, "versionpersistentflag3", "value from default", "version persistent flag 3")
+	versionCmd.PersistentFlags().StringVar(&versionPersistentFlag4, "versionpersistentflag4", "value from default", "version persistent flag 4")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
@@ -107,4 +113,19 @@ func UnmarshalSubMerged(v *viper.Viper, section string, target any) error {
 	// 4. Now unmarshal with proper priority:
 	// flags > env > merged config > defaults
 	return v.Unmarshal(target)
+}
+
+func initConfigVersion() {
+	vprBuf := viper.GetViper()
+	// Bind subcommand-specific cobra flags to viper
+	err := vprBuf.BindPFlags(versionCmd.Flags())
+	if err != nil {
+		logrus.WithField("cobra-cmd", versionCmd.Use).Errorf("error binding flags: %v", err)
+		os.Exit(1)
+	}
+
+	err = UnmarshalSubMerged(vprBuf, versionCmd.Use, &vprFlgsVersion)
+	if err != nil {
+		logrus.Fatalf("failed to unmarshal version config: %v", err)
+	}
 }
